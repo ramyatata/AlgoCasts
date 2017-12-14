406a8cb127e2b83885f6d6e747c7975b9d936cf3
ramyatata <ramyatata007@gmail.com>
2017-12-14 12:28:11 -0800

    palindrome completed

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,55 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome2(str) {
+  //input = string
+  //reverse string and compare
+  //if equal return true
+    // else return false
+  //output = boolean
+
+  const reversedStr = str.split('').reduce((acc, char) => char + acc, '');
+  return str === reversedStr;
+
+  //time = o(n)
+  //space =
+}
+
+function palindrome3(str) {
+  //*********** array method every *************
+
+  //input = string
+  //split array on ''
+  //reverse array
+  //use every method
+    //inside compare each ele equal to last ele and so on
+  //return outpit
+  //output = bool
+
+  return str.split('').every((ele, ind, arr) => ele === str[(str.length - 1) - ind]);
+
+  //time = o(n) double comparision
+  //space =
+}
+
+function palindrome(str) {
+  //************** reducing time complexity ***********
+
+  //loop till half of length of str
+  //compare each ele with mirrored ele
+  //if all comparisions are equal return true
+  //else return false
+
+  let isPalin = true;
+  for(let i = 0; i <= Math.ceil(str.length/2); i++) {
+    if(str[i] !== str[str.length-1-i]) {
+      isPalin = false;
+    }
+  }
+  return isPalin;
+
+  //time = o(n/2) = isreduced by 2
+  //space =
+}
 
 module.exports = palindrome;
