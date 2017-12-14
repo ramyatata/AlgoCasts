02555f9ac53dc52b2fb62d14936c9b910cdd6903
ramyatata <ramyatata007@gmail.com>
2017-12-14 14:10:55 -0800

    maxchar completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //input string
+  //map characters in str o object using reduce
+  //loop though object values
+  //return the key whose value is highest
+  //output = character
+
+  let mostRep = str[0];
+
+  // charMap = {};
+  // for (let char in str) {
+  //   if(!charMap[char]) {
+  //     charMap[char]++;
+  //   } else {
+  //     charMap[char] = 1;
+  //   }
+  // }
+
+  const charMap = str.split('').reduce( (acc, char) => {
+    acc[char] = acc.hasOwnProperty(char) ? acc[char]+1 : 1;
+    return acc;
+  }, {});
+
+  for (var key in obj) {
+    if(obj[key] > obj[mostRep]){
+      mostRep = key;
+    }
+  }
+
+  return mostRep;
+}
+
+maxChar('ab1c1d1e1f1g1');
 
 module.exports = maxChar;
