9f13b041e66758164435861a0214520301505b76
ramyatata <ramyatata007@gmail.com>
2017-12-14 14:20:45 -0800

    fizzbuzz completed

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,29 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  //input number
+  //print - console.log
+  //loop through n
+  //check if divisible by 5 and 3
+    //yes, print 'fizzbuzz'
+  //else if check % by 5
+    //yes, print 'buzz'
+  //else if check % by 3
+    //yes, print 'fizz'
+  //else print number
+
+  for(let i = 1; i <= n; i++ ) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
