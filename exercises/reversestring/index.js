1ed18b1eac74425d16d6086ac986d969f48c44f8
ramyatata <ramyatata007@gmail.com>
2017-12-14 11:24:58 -0800

    completed reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,53 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse2(str) {
+  //input - string
+  //split the string on spaces
+  //reverse the array
+  //join array
+  //output - string
+
+  return str.split('').reverse().join('');
+
+  //time complexity - O(n) = reverse method
+  //space complexity - O(1)
+}
+
+function reverse3(str) {
+  /********   without reverse  *********/
+  //input - string
+  //loop through the input string in reverse order - new es6 loop
+  //add each character to reverse string
+  //return rever string
+  //output - string
+
+  let reverseString = '';
+  for (let char of str) {
+    reverseString = char + reverseString;
+  }
+  return reverseString;
+
+  //time complexity - O(n)
+  //space complexity - O(1)
+}
+
+function reverse(str){
+  //first priority
+  /********   using reduce  *********/
+
+  //input = string
+  //split the str into array
+  //apply reduce function on array
+    // add each char at begining of accumulator
+  //return reverseStrung
+  //output = string
+
+  return str.split('').reduce((acc, char) => char + acc, '' );
+
+  //time - o(n);
+  //space - o(2);
+}
+
 
 module.exports = reverse;
