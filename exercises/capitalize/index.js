56b0f6a464fb206b6c3496515643519c25c205f1
ramyatata <ramyatata007@gmail.com>
2017-12-14 16:39:46 -0800

    capitalize completed

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  //input = string
+  //output = capitalize first char of each word
+
+  //split str on ' '
+  //loop through array
+    //foreach item capitalize first letter
+  //join array on ' ';
+  //return string
+
+  let words = [];
+  for(let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
+
+function capitalize2(str) {
+  //capitalise first chaacter and add it result
+  //loop through the characters starting from 2nd one
+  //if the char left to it is space then capitalize and add it to string
+  //return final string
+
+  var result = str[0].toUpperCase();
+  for(let i = 1; i < str.length; i++) {
+    if(str[i-1] === ' '){
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
