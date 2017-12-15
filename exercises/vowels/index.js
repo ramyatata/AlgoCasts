dfb0503406abd95a9cd526c6d542292ec0f02b1e
ramyatata <ramyatata007@gmail.com>
2017-12-15 12:33:40 -0800

    vowels completed

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,35 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels2(str) {
+  //input = string
+  //output = count
+  //create an array for vowels
+  //create a counter variable
+  //convert the input string to lowercase
+  //loop through the string characters
+    //if character matches vowel in array
+       //increement count by 1
+  //return counter
+
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let counter = 0;
+  const strLower = str.toLowerCase();
+
+  for (let char of strLower) {
+    if (vowels.includes(char)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+function vowels(str) {
+//*****************  Regular expression ****************** /
+
+  const matches = str.match(/[aeiou]/gi);   //return array of matched eles, if nothing found return null
+  return matches ? matches.length : 0 ;
+}
 
 module.exports = vowels;
