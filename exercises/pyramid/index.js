0500af668043f96fefa403e048fa2f1cab3e6957
ramyatata <ramyatata007@gmail.com>
2017-12-15 12:19:49 -0800

    pyramid completed

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,67 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+function pyramid2(n) {
+  //input = lines of pyramid
+  //output- pyramid with n lines
+  //loop from 0 to n - rows
+    //create a empty string level
+    //iterate through columns 0 to 2n-1
+    //check condition to display # - FIND THE MIDPOINT OF 2N-1 AND CURRENT ROW = NO.OF '#' TO BE ADDED.
+    //calculate upperlimit and lower limit
+      //if cols is in range add '#'
+      //else add ' '
+    // log level on console
+
+  let midpoint = Math.floor((2*n - 1)/2);
+
+  for (let rows = 0; rows < n; rows++) {
+    let level = '';
+    let upperIndex = midpoint + rows;
+    let lowerIndex = midpoint - rows;
+
+    for (let cols = 0; cols < 2*n - 1 ; cols++) {
+      if (cols >= lowerIndex && cols <= upperIndex) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
+
+function pyramid(n, rows = 0, level='') {
+  //***************   recursion  ***************/
+
+  if (rows === n) {
+    return;
+  }
+
+  if (level.length === (2*n - 1)) {
+    console.log(level);
+    rows++;
+    return pyramid(n, rows);
+  }
+
+  let midpoint = math.floor((2*n - 1)/2);
+  let upperlimit = midpoint + rows;
+  let lowerlimit = midpoint - rows;
+
+  if (level.length >= lowerlimit && level.length <= upperlimit){
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  pyramid(n, rows, level)
+
+}
 
 module.exports = pyramid;
