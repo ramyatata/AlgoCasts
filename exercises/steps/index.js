e138f86b22195fe6cf563fe7362dec61668895e9
ramyatata <ramyatata007@gmail.com>
2017-12-15 10:57:16 -0800

    steps completed

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps2(n) {
+  //input is  size of letter and no.of lines
+  //from 0 to n iterate through rows
+    //create a new empty string stairs
+    //from o to n iterate through columns
+      //if columns is <= rows
+        //add '#' to stairs
+      //else add ' ' to stairs
+    //log stairs on console
+
+  for (let rows = 0; rows < n; rows++) {
+    let stairs = '';
+    for (let cols = 0; cols < n; cols++) {
+      if (cols <= rows) {
+        stairs += '#';
+      } else {
+        stairs +=  ' ';
+      }
+    }
+    console.log(stairs);
+  }
+}
+
+function steps(n, rows = 0, stairs = '') {
+  /***************  recursive solution *******************/
+  if (n === rows) {
+    return;
+  }
+
+  if(stairs.length === n) {
+    console.log(stairs);
+    stairs = '';
+    rows++;
+  }
+
+  if (stairs.length <= rows) {
+    stairs += '#';
+  } else {
+    stairs += ' ';
+  }
+
+  steps(n, rows, stairs)
+}
 
 module.exports = steps;
